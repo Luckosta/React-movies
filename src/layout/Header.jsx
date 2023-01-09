b923eec6349fe47696ab61aeaf8bec34a5c27090
Leonid Safonov <safonov.lenia2017@yandex.ru>
2023-01-09 13:45:11 +0300

    initial

---
+++ src/layout/Header.jsx
@@ -0,0 +1,16 @@
+
+function Header() {
+	return (
+		<nav>
+			<div className="nav-wrapper">
+				<a href="#" className="brand-logo center">React Movies</a>
+				<ul id="nav-mobile" className="left hide-on-med-and-down">
+					<li><a href="#">Something</a></li>
+
+				</ul>
+			</div>
+		</nav>
+	)
+}
+
+export default Header
